a9de76fb2ac05e180a8902b3bc435dab1c63bb21
Brendan Ross <br3ross@gmail.com>
2017-09-17 04:03:28 -0400

    Set model to tiny-yolo

--- process_video.sh
+++ process_video.sh
@@ -3,14 +3,15 @@
 VIDEO=$1
 FRAMERATE=1
 OUTDIR=ffmpeg-out
+MODEL=tiny-yolo-voc
 
 mkdir -p $OUTDIR
 ffmpeg -i $VIDEO -vf fps=$FRAMERATE $OUTDIR/out%d.png
 
 cd darkflow
 flow --imgdir ../$OUTDIR \
-     --model cfg/yolo.cfg \
-     --load bin/yolo.weights --json
+     --model cfg/$MODEL.cfg \
+     --load bin/$MODEL.weights --json
 
 cd ..
 FRAMECOUNT=$(ls -1q $OUTDIR/out/out* | wc -l)
