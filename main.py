f41a288fc6b4bb92d42b0b3d138c3c7439af8297
StefanSokic <stefan.sokic33@gmail.com>
2017-09-17 01:38:08 -0400

    Basic backend is done. Ayyeee

---
+++ main.py
@@ -0,0 +1,45 @@
+#!flask/bin/python
+from flask import Flask, jsonify, request, redirect, url_for
+from flask import request
+from flask import abort
+from werkzeug import secure_filename
+import os
+
+UPLOAD_FOLDER = './uploads/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mp4', 'wav', 'avi'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # INSERT THE CALL TO THE BASH SCRIPT, RUN THROUGH DARKFLOW AND 
+            return "Photo uploaded"
+            #return redirect(url_for('uploaded_file',filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+if __name__ == '__main__':
+    app.run(debug=True)
